7bd1d7f4741de85df5b587a8cd64b6b26ffb4eb0
Tahsin Ahmed <tahsahmed@cs.stonybrook.edu>
2024-04-02 21:15:06 -0400

    spring cleaning

--- scripts/install_rust.sh
+++ scripts/install_rust.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
 
+# Install dependencies
 sudo apt update
+sudo apt install build-essential libssl-dev pkg-config
 
 # Install rust
-sudo apt install build-essential libssl-dev pkg-config -y
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y # install unattended
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
 
+# Install cranelift
 rustup toolchain install nightly
 rustup default nightly
 rustup component add rustc-codegen-cranelift-preview --toolchain nightly
+
+# Install mold
 MOLD_RELEASE=https://github.com/rui314/mold/releases/download/v2.30.0/mold-2.30.0-x86_64-linux.tar.gz
 wget -O - "$MOLD_RELEASE" | sudo tar -C /usr/local --strip-components=1 --no-overwrite-dir -xzf -
 
-# build in background
-cargo +nightly build --profile=fast-dev-linux &
-cargo build --release &
-
-echo Run 'source \"\$HOME/.cargo/env\"'
-echo Add a config.toml to run the server
+# Build in background
+# cargo +nightly build --profile=fast-dev-linux &
+# cargo build --release &
