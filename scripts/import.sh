66f87eb4effc9d796ccc4bdcd02a5dbc8db803eb
Tahsin Ahmed <tahsahmed@cs.stonybrook.edu>
2024-04-06 15:35:27 -0400

    import db works

--- scripts/import.sh
+++ scripts/import.sh
@@ -2,28 +2,38 @@
 
 SCRIPT_DIR=$(dirname "$0")
 
+function usage() {
+    echo "Usage: $0 region"
+    echo "  region: The region to import."
+}
+
+if [ "$#" -ne 1 ]; then
+    usage
+    exit 1
+fi
+
 # Download the data
-OSM_PBF_FILE="us-northeast.osm.pbf"
-if [ ! -f "/data/${OSM_PBF_FILENAME}" ]; then
-    wget -P /data "https://grading.cse356.compas.cs.stonybrook.edu/data/${OSM_PBF_FILENAME}"
+REGION=$1
+if [ ! -f "/data/${REGION}.osm.pbf" ]; then
+    wget -P /data "https://grading.cse356.compas.cs.stonybrook.edu/data/${REGION}.osm.pbf"
 fi
 
 ###########################################################################################################
 
-# Create volumes for tiles
-docker volume create osm-data
-docker volume create osm-tiles
+# # Create volumes for tiles
+# docker volume create osm-data
+# docker volume create osm-tiles
 
-# Import the node data
-docker run \
-    -p 8081:80 \
-    -p 5432:5432 \
-    -v /data/${OSM_PBF_FILE}:/data/region.osm.pbf \
-    -v osm-data:/data/database/ \
-    -v osm-tiles:/data/tiles/ \
-    --shm-size="3gb" \
-    overv/openstreetmap-tile-server \
-    import
+# # Import the node data
+# docker run \
+#     -p 8081:80 \
+#     -p 5432:5432 \
+#     -v /data/${OSM_PBF_FILE}:/data/region.osm.pbf \
+#     -v osm-data:/data/database/ \
+#     -v osm-tiles:/data/tiles/ \
+#     --shm-size="3gb" \
+#     overv/openstreetmap-tile-server \
+#     import
 
 ###########################################################################################################
 
@@ -31,14 +41,11 @@ docker run \
 docker volume create osm-routing
 
 # Build the routing service
-docker build -t services/routing $SCRIPT_DIR/../services/routing 
+docker build -t import_routing $SCRIPT_DIR/../services/routing 
 
 # Import the routing data
-docker run \
+docker run --rm \
     -e POSTGRES_PASSWORD=postgres \
-    -p 5433:5432 \
-    -v /data/${OSM_PBF_FILE}:/data/region.osm.pbf \
+    -v /data/${REGION}.osm.pbf:/data/region.osm.pbf \
     -v osm-routing:/var/lib/postgresql/data \
-    --shm-size="3gb" \
-    services/routing \
-    import
+    import_routing
--- services/routing/run.sh
+++ services/routing/run.sh
@@ -2,53 +2,29 @@
 
 set -euo pipefail
 
-function setPostgresPassword() {
-    sudo -u postgres psql -c "ALTER USER router PASSWORD '${PGRPASSWORD:-router}'"
-}
-
-if [ "$1" == "import" ]; then
-    # Initialize PostgreSQL
-    service postgresql start
-    sudo -u postgres createuser router
-    sudo -u postgres createdb -E UTF8 -O router routing 
-    sudo -u postgres psql -d routing -c "CREATE EXTENSION postgis;"
-    sudo -u postgres psql -d routing -c "CREATE EXTENSION pgrouting;"
-    sudo -u postgres psql -d routing -c "ALTER TABLE spatial_ref_sys OWNER TO router;"
-    setPostgresPassword
-
-    # Download osm2po (http://osm2po.de/)
-    wget -P /tmp/osm2po https://osm2po.de/releases/osm2po-5.5.11.zip
-    unzip -o /tmp/osm2po/osm2po-5.5.11.zip -d /tmp/osm2po 
-    cp /osm2po.config /tmp/osm2po/osm2po.config
-
-    # Generate topology
-    java -jar /tmp/osm2po/osm2po-core-5.5.11-signed.jar cmd=c /data/region.osm.pbf 
-
-    # Import data
-    psql -U router -d routing -q -f /tmp/osm2po/osm2po_4pgr.sql
-
-    # Cleanup
-    rm -rf /tmp/osm2po
-    service postgresql stop
-    exit 0
+# Start PostgreSQL
+DB_DIR=/var/lib/postgresql/data
+mkdir -p $DB_DIR 
+chown -R postgres: /var/lib/postgresql
+if [ ! -f $DB_DIR/PG_VERSION ]; then
+    sudo -u postgres /usr/lib/postgresql/$PG_VERSION/bin/pg_ctl -D $DB_DIR initdb -o "--locale C.UTF-8"
 fi
+sudo -u postgres /usr/lib/postgresql/$PG_VERSION/bin/pg_ctl -D $DB_DIR start
 
-if [ "$1" == "run" ]; then
-    # Start PostgreSQL
-    service postgresql start
-    setPostgresPassword
-        
-    # Run while handling docker stop's SIGTERM
-    stop_handler() {
-        kill -TERM "$child"
-    }
-    trap stop_handler SIGTERM
+# Create tables
+sudo -u postgres createuser router
+sudo -u postgres createdb -E UTF8 -O router routing 
+sudo -u postgres psql -d routing -c "CREATE EXTENSION postgis;"
+sudo -u postgres psql -d routing -c "CREATE EXTENSION pgRouting;"
+sudo -u postgres psql -d routing -c "ALTER TABLE spatial_ref_sys OWNER TO router;"
+sudo -u postgres psql -c "ALTER USER router PASSWORD '${PGPASSWORD:-router}'"
 
-    sudo -u renderer renderd -f -c /etc/renderd.conf &
-    child=$!
-    wait "$child"
+# Download osm2po (http://osm2po.de/)
+wget https://osm2po.de/releases/osm2po-5.5.11.zip
+unzip -n osm2po-5.5.11.zip
 
-    service postgresql stop
+# Generate topology
+java -jar osm2po-core-5.5.11-signed.jar cmd=c /data/region.osm.pbf 
 
-    exit 0
-fi
+# Import data
+sudo -u postgres psql -U router -d routing -q -f osm/osm_2po_4pgr.sql
