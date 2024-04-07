8cd70115fdeb5052e6d27183f23948f599f8393c
Jacky Xie <jackyxie2520@outlook.com>
2024-04-07 04:04:18 -0400

    Make postgres config file

--- services/database/run.sh
+++ services/database/run.sh
@@ -9,7 +9,10 @@ chown -R postgres: /var/lib/postgresql
 if [ ! -f $DB_DIR/PG_VERSION ]; then
     sudo -u postgres /usr/lib/postgresql/$PG_VERSION/bin/pg_ctl -D $DB_DIR initdb -o "--locale C.UTF-8"
 fi
+echo "include = '/postgresql.conf'" >> /var/lib/postgresql/data/postgresql.conf
 sudo -u postgres /usr/lib/postgresql/$PG_VERSION/bin/pg_ctl -D $DB_DIR start
+
+#sudo -u postgres psql -d gis -c "SELECT pg_reload_conf();"
 
 # Create tables
 sudo -u postgres createuser carto
@@ -33,13 +36,13 @@ osm2pgsql -d gis -U carto --create --slim -G --hstore --number-processes ${THREA
 # sudo -u postgres psql -c "ALTER USER router PASSWORD '${PGPASSWORD_ROUTER:-router}'"
 
 # Download osm2po (http://osm2po.de/)
-wget https://osm2po.de/releases/osm2po-5.5.11.zip
-unzip -o osm2po-5.5.11.zip
-echo "postp.0.class = de.cm.osm2po.plugins.postp.PgRoutingWriter" >> osm2po.config
-echo "wtr.finalMask = car,foot,bike" >> osm2po.config
-
-# Generate topology
-java -jar osm2po-core-5.5.11-signed.jar cmd=c /data/region.osm.pbf 
-
-# Import routing data
-sudo -u postgres psql -U carto -d gis -q -f osm/osm_2po_4pgr.sql
+#wget https://osm2po.de/releases/osm2po-5.5.11.zip
+#unzip -o osm2po-5.5.11.zip
+#echo "postp.0.class = de.cm.osm2po.plugins.postp.PgRoutingWriter" >> osm2po.config
+#echo "wtr.finalMask = car,foot,bike" >> osm2po.config
+#
+## Generate topology
+#java -jar osm2po-core-5.5.11-signed.jar cmd=c /data/region.osm.pbf 
+#
+## Import routing data
+#sudo -u postgres psql -U carto -d gis -q -f osm/osm_2po_4pgr.sql
