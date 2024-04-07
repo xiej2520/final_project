bc5c6d8030ff149d9315d447a170aee255a5986f
Jacky Xie <jackyxie2520@outlook.com>
2024-04-07 04:16:51 -0400

    Uncomment osm2po

--- services/database/run.sh
+++ services/database/run.sh
@@ -36,13 +36,13 @@ osm2pgsql -d gis -U carto --create --slim -G --hstore --number-processes ${THREA
 # sudo -u postgres psql -c "ALTER USER router PASSWORD '${PGPASSWORD_ROUTER:-router}'"
 
 # Download osm2po (http://osm2po.de/)
-#wget https://osm2po.de/releases/osm2po-5.5.11.zip
-#unzip -o osm2po-5.5.11.zip
-#echo "postp.0.class = de.cm.osm2po.plugins.postp.PgRoutingWriter" >> osm2po.config
-#echo "wtr.finalMask = car,foot,bike" >> osm2po.config
-#
-## Generate topology
-#java -jar osm2po-core-5.5.11-signed.jar cmd=c /data/region.osm.pbf 
-#
-## Import routing data
-#sudo -u postgres psql -U carto -d gis -q -f osm/osm_2po_4pgr.sql
+wget https://osm2po.de/releases/osm2po-5.5.11.zip
+unzip -o osm2po-5.5.11.zip
+echo "postp.0.class = de.cm.osm2po.plugins.postp.PgRoutingWriter" >> osm2po.config
+echo "wtr.finalMask = car,foot,bike" >> osm2po.config
+
+# Generate topology
+java -jar osm2po-core-5.5.11-signed.jar cmd=c /data/region.osm.pbf 
+
+# Import routing data
+sudo -u postgres psql -U carto -d gis -q -f osm/osm_2po_4pgr.sql
