355e123ef9b4be592679304afab08bbb50838bcd
Jacky Xie <jackyxie2520@outlook.com>
2024-04-09 19:12:13 -0400

    add map_controller.rs change

--- src/controllers/search_controller.rs
+++ src/controllers/search_controller.rs
@@ -60,34 +60,40 @@ impl From<tokio_postgres::Row> for AnywhereObject {
 }
 
 const BBOX_SQL_QUERY: &str = r#"
-    WITH transformed_bbox AS (
-        SELECT ST_Transform(ST_MakeEnvelope($1, $2, $3, $4, 4326), 3857) AS bbox
-    )
+WITH transformed_bbox AS (
+    SELECT ST_Transform(ST_MakeEnvelope($1, $2, $3, $4, 4326), 3857) AS bbox
+)
+SELECT 
+    name,
+    ST_Y(ST_Transform(ST_Centroid(intersection_geom), 4326)) AS lat,
+    ST_X(ST_Transform(ST_Centroid(intersection_geom), 4326)) AS lon,
+    ST_Distance(ST_Centroid(intersection_geom), ST_Centroid(transformed_bbox.bbox)) AS distance_to_center
+FROM (
     SELECT 
         name,
-        ST_Y(ST_Transform(ST_Centroid(intersection_geom), 4326)) AS lat,
-        ST_X(ST_Transform(ST_Centroid(intersection_geom), 4326)) AS lon,
-        ST_Distance(ST_Centroid(intersection_geom), ST_Centroid(transformed_bbox.bbox)) AS distance_to_center
+        ST_Intersection(way, transformed_bbox.bbox) AS intersection_geom
     FROM (
-        SELECT 
-            name,
-            ST_Intersection(way, transformed_bbox.bbox) AS intersection_geom
-        FROM (
-            (SELECT name, way FROM planet_osm_polygon WHERE name LIKE $5 LIMIT 30)
-            UNION ALL
-            (SELECT name, way FROM planet_osm_line WHERE name LIKE $5 LIMIT 30)
-            UNION ALL
-            (SELECT name, way FROM planet_osm_roads WHERE name LIKE $5 LIMIT 30)
-            UNION ALL
-            (SELECT name, way FROM planet_osm_point WHERE name LIKE $5 LIMIT 30)
-        ) AS relevant_tables, transformed_bbox
-        WHERE 
-            way && transformed_bbox.bbox
-    ) AS intersection_query, transformed_bbox
-    WHERE NOT ST_IsEmpty(intersection_geom)
-    ORDER BY 
-        distance_to_center
-    LIMIT 30;
+        (SELECT name, way FROM planet_osm_polygon
+          WHERE name LIKE $5 AND ST_Intersects(way, (SELECT bbox FROM transformed_bbox))
+          LIMIT 30
+        ) UNION ALL
+        (SELECT name, way FROM planet_osm_line
+          WHERE name LIKE $5 AND ST_Intersects(way, (SELECT bbox FROM transformed_bbox))
+          LIMIT 30
+        ) UNION ALL
+        (SELECT name, way FROM planet_osm_roads
+          WHERE name LIKE $5 AND ST_Intersects(way, (SELECT bbox FROM transformed_bbox))
+          LIMIT 30
+        ) UNION ALL
+        (SELECT name, way FROM planet_osm_point
+          WHERE name LIKE $5 AND ST_Intersects(way, (SELECT bbox FROM transformed_bbox))
+          LIMIT 30
+        )
+    ) AS relevant_tables, transformed_bbox
+) AS intersection_query, transformed_bbox
+ORDER BY 
+    distance_to_center
+LIMIT 30;
 "#;
 
 pub async fn search_in_bbox(
@@ -101,6 +107,9 @@ pub async fn search_in_bbox(
     search_term: &str,
 ) -> Result<Vec<InBBoxObject>, tokio_postgres::Error> {
     let stmt = client.prepare(BBOX_SQL_QUERY).await?;
+    // match any containing, build an index
+    let search_term = format!("%{search_term}%");
+    tracing::info!("Searching for {search_term}");
 
     let rows = client
         .query(
@@ -113,24 +122,24 @@ pub async fn search_in_bbox(
 }
 
 const ANYWHERE_SQL_QUERY: &str = r#"
-    SELECT 
-        name,
-        ST_Y(ST_Transform(ST_Centroid(way), 4326)) AS centroidLat,
-        ST_X(ST_Transform(ST_Centroid(way), 4326)) AS centroidLon,
-        ST_YMin(ST_Transform(ST_Envelope(way), 4326)) AS minLat,
-        ST_XMin(ST_Transform(ST_Envelope(way), 4326)) AS minLon,
-        ST_YMax(ST_Transform(ST_Envelope(way), 4326)) AS maxLat,
-        ST_XMax(ST_Transform(ST_Envelope(way), 4326)) AS maxLon
-    FROM (
-        SELECT name, way FROM planet_osm_polygon WHERE name LIKE $1
-        UNION ALL
-        SELECT name, way FROM planet_osm_line WHERE name LIKE $1
-        UNION ALL
-        SELECT name, way FROM planet_osm_roads WHERE name LIKE $1
-        UNION ALL
-        SELECT name, way FROM planet_osm_point WHERE name LIKE $1
-    ) AS relevant_tables
-    LIMIT 30;
+SELECT 
+    name,
+    ST_Y(ST_Transform(ST_Centroid(way), 4326)) AS centroidLat,
+    ST_X(ST_Transform(ST_Centroid(way), 4326)) AS centroidLon,
+    ST_YMin(ST_Transform(ST_Envelope(way), 4326)) AS minLat,
+    ST_XMin(ST_Transform(ST_Envelope(way), 4326)) AS minLon,
+    ST_YMax(ST_Transform(ST_Envelope(way), 4326)) AS maxLat,
+    ST_XMax(ST_Transform(ST_Envelope(way), 4326)) AS maxLon
+FROM (
+    SELECT name, way FROM planet_osm_polygon WHERE name LIKE $1
+    UNION ALL
+    SELECT name, way FROM planet_osm_line WHERE name LIKE $1
+    UNION ALL
+    SELECT name, way FROM planet_osm_roads WHERE name LIKE $1
+    UNION ALL
+    SELECT name, way FROM planet_osm_point WHERE name LIKE $1
+) AS relevant_tables
+LIMIT 30;
 "#;
 
 pub async fn search_anywhere(
@@ -138,6 +147,8 @@ pub async fn search_anywhere(
     search_term: &str,
 ) -> Result<Vec<AnywhereObject>, tokio_postgres::Error> {
     let stmt = client.prepare(ANYWHERE_SQL_QUERY).await?;
+    let search_term = format!("%{search_term}%");
+    tracing::info!("Searching for {search_term}");
 
     let rows = client.query(&stmt, &[&search_term]).await?;
 
