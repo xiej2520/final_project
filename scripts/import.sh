4fc3b68c68b301e394450dd67808790f8d72064b
Tahsin Ahmed <tahsahmed@cs.stonybrook.edu>
2024-04-18 22:33:08 -0400

    add freeze remove flatnodes

--- scripts/import.sh
+++ scripts/import.sh
@@ -23,11 +23,14 @@ docker volume create nominatim-flatnode
 
 docker run --rm \
   -v /data:/data \
-  -v nominatim-data:/var/lib/postgresql/14/main \
+  -v nominatim-data2:/var/lib/postgresql/14/main \
   -v nominatim-flatnode:/nominatim/flatnode \
   -e PBF_PATH="/data/${PBF_NAME}" \
+  -e FREEZE=true \
   mediagis/nominatim:4.4 \
   /bin/bash -c "/app/config.sh && useradd -m nominatim && /app/init.sh && touch ${IMPORT_FINISHED}"
+
+docker volume rm nominatim-flatnode # flatnodes unneeded after import
 
 # Create plane tiles
 docker run --rm \
