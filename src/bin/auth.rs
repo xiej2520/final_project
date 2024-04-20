655ae591f228ea36acb737c95ca2228104a1a5e9
Jacky Xie <jackyxie2520@outlook.com>
2024-04-20 13:18:14 -0400

    Fix auth compile bugs

--- src/bin/auth.rs
+++ src/bin/auth.rs
@@ -1,21 +1,23 @@
 use std::net::SocketAddr;
+use std::sync::Arc;
 
 use axum::Router;
 
+use tokio::sync::RwLock;
 use tower::ServiceBuilder;
 
 use tower_http::trace::TraceLayer;
 
 use server::{append_headers, init_logging, print_request_response};
+use server::controllers::user_controller;
 use server::CONFIG;
-use server::http_client::HttpClient;
 use server::routers::*;
 
 /// Runs an user creation, login, and authentication service 
 #[tokio::main]
 async fn main() {
     init_logging();
-    let user_store = user_controller::UserStore::default();
+    let user_store = Arc::new(RwLock::new(user_controller::UserStore::default()));
 
     let mut auth_app = Router::new()
         .nest("/auth", auth_router::new_router())
